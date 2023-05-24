b77ff93d765ed1d408ef0740753a665cd6eb7d57
LugpDev2022 <lugpdev2022@gmail.com>
2023-05-24 10:37:53 -0600

    Finish navbar styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,31 @@
 'use client';
 
+import { useState } from 'react';
+
 import Image from 'next/image';
 import Link from 'next/link';
 import logo from '../../public/logo.svg';
 import menuIcon from '../../public/menu.png';
-import { useState } from 'react';
+
+interface NavLinkProps {
+  children: React.ReactNode;
+  href: string;
+}
+
+const NavLink: React.FC<NavLinkProps> = ({ children, href }) => (
+  <li className='mt-4 sm:mt-0 flex flex-col justify-center'>
+    <Link className='hover:text-[#4C837A] transition duration-200' href={href}>
+      {children}
+    </Link>
+  </li>
+);
 
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState<boolean>(false);
 
   return (
-    <nav className='bg-white p-3 fixed w-full top-0 left-0'>
-      <div className='flex justify-between items-center'>
+    <nav className='bg-white p-3 sm:px-8 fixed w-full top-0 left-0 sm:flex sm:h-[72px]'>
+      <div className='flex justify-between sm:me-16'>
         <Link href='/' className='w-12'>
           <Image alt='Logo' src={logo} />
         </Link>
@@ -27,23 +41,19 @@ const Navbar = () => {
       <ul
         className={`
           ${showNavbar ? 'block' : 'hidden'} 
-          sm:block
+          sm:inline-flex
           text-[#85BAB1] 
           font-bold 
           text-lg 
           text-center
+          sm:flex-wrap
+          sm:gap-8
         `}
       >
-        <hr className='border-[1.5px] my-4 border-[#85BAB1]' />
-        <li>
-          <Link href='/'>About</Link>
-        </li>
-        <li>
-          <Link href='/projects'>Projects</Link>
-        </li>
-        <li>
-          <Link href='/contact'>Contact</Link>
-        </li>
+        <hr className='border-[1.5px] my-4 border-[#85BAB1] sm:hidden' />
+        <NavLink href='/'>About</NavLink>
+        <NavLink href='projects'>Projects</NavLink>
+        <NavLink href='contact'>Contact</NavLink>
       </ul>
     </nav>
   );
