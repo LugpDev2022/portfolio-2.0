6a3788becf203bf3437faf6b09668653793dbacc
LugpDev2022 <lugpdev2022@gmail.com>
2023-05-24 11:00:20 -0600

    Change active link color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,30 +1,56 @@
 'use client';
 
 import { useState } from 'react';
+import { usePathname } from 'next/navigation';
 
 import Image from 'next/image';
 import Link from 'next/link';
+
 import logo from '../../public/logo.svg';
 import menuIcon from '../../public/menu.png';
 
 interface NavLinkProps {
   children: React.ReactNode;
   href: string;
+  path: string;
 }
 
-const NavLink: React.FC<NavLinkProps> = ({ children, href }) => (
+const NavLink: React.FC<NavLinkProps> = ({ children, href, path }) => (
   <li className='mt-4 sm:mt-0 flex flex-col justify-center'>
-    <Link className='hover:text-[#4C837A] transition duration-200' href={href}>
+    <Link
+      className={`
+        ${path === href ? 'text-[#4C837A]' : 'text-[#85BAB1]'}
+        hover:text-[#4C837A] 
+        transition 
+        duration-200
+      `}
+      href={href}
+    >
       {children}
     </Link>
   </li>
 );
 
+//TODO: Add animation when showing mobile navbar
 const Navbar = () => {
   const [showNavbar, setShowNavbar] = useState<boolean>(false);
+  const path = usePathname();
 
   return (
-    <nav className='bg-white p-3 sm:px-8 fixed w-full top-0 left-0 sm:flex sm:h-[72px]'>
+    <nav
+      className='
+        bg-white 
+        left-0 
+        p-3 
+        shadow-black/25 
+        shadow-md 
+        sm:flex 
+        sm:h-[72px]
+        sm:px-8 
+        top-0 
+        w-full
+      '
+    >
       <div className='flex justify-between sm:me-16'>
         <Link href='/' className='w-12'>
           <Image alt='Logo' src={logo} />
@@ -41,19 +67,24 @@ const Navbar = () => {
       <ul
         className={`
           ${showNavbar ? 'block' : 'hidden'} 
-          sm:inline-flex
-          text-[#85BAB1] 
           font-bold 
-          text-lg 
-          text-center
           sm:flex-wrap
           sm:gap-8
+          sm:inline-flex 
+          text-center
+          text-lg 
         `}
       >
         <hr className='border-[1.5px] my-4 border-[#85BAB1] sm:hidden' />
-        <NavLink href='/'>About</NavLink>
-        <NavLink href='projects'>Projects</NavLink>
-        <NavLink href='contact'>Contact</NavLink>
+        <NavLink path={path} href='/'>
+          About
+        </NavLink>
+        <NavLink path={path} href='/projects'>
+          Projects
+        </NavLink>
+        <NavLink path={path} href='/contact'>
+          Contact
+        </NavLink>
       </ul>
     </nav>
   );
