f09953ac858d37ca8eea5b68ea8d14ff9f161534
LugpDev2022 <lugpdev2022@gmail.com>
2023-05-24 13:05:24 -0600

    About me part

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,23 @@
 import AboutSectionContainer from '@/components/AboutSectionContainer';
 import Image from 'next/image';
 import profilePhoto from '../../public/profile.png';
+import aboutImage from '../../public/about.png';
 
 export default function Home() {
   return (
     <>
-      <AboutSectionContainer>
+      <div
+        className='
+          flex
+          flex-col
+          gap-12
+          items-center
+          justify-center
+          py-6
+          sm:flex-row
+          sm:py-12
+        '
+      >
         <div className='bg-[#C6BF88] rounded-full overflow-hidden h-[250px] w-[250px]'>
           <Image alt='Photo' src={profilePhoto} />
         </div>
@@ -17,9 +29,23 @@ export default function Home() {
           </h1>
           <h2 className='text-xl'>Fronted Web Developer</h2>
         </div>
-      </AboutSectionContainer>
+      </div>
 
-      <div></div>
+      <AboutSectionContainer background='#C6BF88'>
+        <div className='sm:basis-2/3 md:basis-3/5 xl:basis-1/2 2xl:basis-1/3'>
+          <h2 className='subtitle'>About Me</h2>
+          <p className='text-white'>
+            Hi! I&apos;m Luis, a frontend developer born in Mexico. Since I was
+            in elementary school, I knew I wanted to dedicate my life to
+            technology. My dream is to someday work for a big tech company, so
+            I&apos;m actively working towards it. I would also be grateful to
+            help you fulfill your dream.
+          </p>
+        </div>
+        <div>
+          <Image src={aboutImage} alt='About Image' />
+        </div>
+      </AboutSectionContainer>
     </>
   );
 }
--- src/components/AboutSectionContainer.tsx
+++ src/components/AboutSectionContainer.tsx
@@ -1,25 +1,38 @@
 interface Props {
   children: React.ReactNode;
   className?: string;
-  alternativeBackground?: boolean;
+  background?: string;
+  orientation?: 'left' | 'right';
 }
 
 const AboutSectionContainer: React.FC<Props> = ({
   children,
   className = '',
-  alternativeBackground,
+  background,
+  orientation = 'left',
 }) => (
   <div
     className={`
-      ${alternativeBackground ? `bg-${alternativeBackground}` : ''}
       ${className}
+      ${background ? `bg-[${background}]` : ''}
+      ${orientation === 'left' ? 'sm:text-left' : 'sm:text-right'}
       flex
       flex-col
-      gap-12
+      gap-5
       items-center
-      justify-center
+      justify-between
+      p-5
       py-6
       sm:flex-row
+      sm:px-10
+      sm:py-12
+      md:px-14
+      lg:px-24
+      xl:px-36
+      2xl:justify-center
+      2xl:gap-20
+      text-center
+      text-xl
     `}
   >
     {children}
