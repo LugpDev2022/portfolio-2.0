8f391c42f396f45a3b647480e7087679a045fc0a
LugpDev2022 <lugpdev2022@gmail.com>
2023-05-27 18:51:38 -0600

    Create the footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import Navbar from '@/components/Navbar';
 import '../styles/globals.css';
 import { Inter } from 'next/font/google';
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={`${inter.className} mt-[72px]`}>
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,10 @@
+const Footer = () => (
+  <div className='bg-[#24403B] text-center text-white py-2'>
+    <span>
+      Developed and designed by Luis Alberto Cervantes García. ©2023. All rights
+      reserved.
+    </span>
+  </div>
+);
+
+export default Footer;
