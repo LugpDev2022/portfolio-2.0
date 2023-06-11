9973bf829abaf7f81214d76584051cecfff20baf
LugpDev2022 <lugpdev2022@gmail.com>
2023-06-11 08:07:13 -0600

    Catch form errors

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,12 +1,61 @@
+'use client';
+
+import { useForm } from '@/hooks/useForm';
+
 const ContactForm = () => {
+  const { onInputChange, onResetForm, formState } = useForm({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleSubmit = async (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const errors = {
+      email: '',
+      name: '',
+      message: '',
+    };
+
+    const { name, email, message }: any = formState;
+
+    if (!name) errors.name = 'Name is required';
+
+    if (!email) errors.email = 'Email is required';
+    else if (!email.includes('@')) errors.email = 'Email is not valid';
+
+    if (!message) errors.message = 'Message is required';
+    else if (message.length <= 5) errors.message = 'Message is too short';
+
+    console.log(errors);
+  };
+
   return (
-    <form className='flex flex-col gap-7 justify-around md:col-span-2 h-full'>
-      <input type='text' className='contact-input' placeholder='Full Name' />
-      <input type='email' className='contact-input' placeholder='Email' />
+    <form
+      onSubmit={handleSubmit}
+      className='flex flex-col gap-7 justify-around md:col-span-2 h-full'
+    >
+      <input
+        type='text'
+        className='contact-input'
+        placeholder='Full Name'
+        name='name'
+        onChange={onInputChange}
+      />
+      <input
+        type='email'
+        className='contact-input'
+        placeholder='Email'
+        name='email'
+        onChange={onInputChange}
+      />
       <textarea
         rows={10}
         className='contact-input py-2'
         placeholder='Your Message'
+        name='message'
+        onChange={onInputChange}
       ></textarea>
 
       <button
