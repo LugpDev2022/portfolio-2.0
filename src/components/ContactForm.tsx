849cba976b0ebd596c77089d8c07b991675acb14
LugpDev2022 <lugpdev2022@gmail.com>
2023-06-11 15:05:34 -0600

    Replace useForm with useFormik

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,89 +1,79 @@
 'use client';
 
-import { getFormErrors } from '@/helpers/getFormErrors';
-import { useForm } from '@/hooks/useForm';
-import { useState } from 'react';
+import { Field, Formik } from 'formik';
 
 const ContactForm = () => {
-  const [formErrors, setFormErrors] = useState({
-    name: '',
-    email: '',
-    message: '',
-  });
-  const { onInputChange, formState } = useForm({
-    name: '',
-    email: '',
-    message: '',
-  });
-
-  const handleSubmit = async (e: React.SyntheticEvent) => {
-    e.preventDefault();
-
-    const errors = getFormErrors(formState);
-    setFormErrors(errors);
-  };
-
   return (
-    <form
-      onSubmit={handleSubmit}
-      className='flex flex-col gap-7 justify-around md:col-span-2 h-full'
+    <Formik
+      initialValues={{
+        name: '',
+        email: '',
+        message: '',
+      }}
+      onSubmit={(values) => console.log(values)}
     >
-      <div className='relative'>
-        <label className='input-label'>Name</label>
-        <input
-          type='text'
-          className='contact-input'
-          placeholder='Your name'
-          name='name'
-          onChange={onInputChange}
-        />
-        <span className='absolute right-1 -top-5 text-sm text-red-600 font-semibold'>
-          {formErrors.name}
-        </span>
-      </div>
-      <div className='relative'>
-        <label className='input-label'>Email</label>
-        <input
-          type='email'
-          className='contact-input'
-          placeholder='email@example.com'
-          name='email'
-          onChange={onInputChange}
-        />
-        <span className='absolute right-1 -top-5 text-sm text-red-600 font-semibold'>
-          {formErrors.email}
-        </span>
-      </div>
-      <div className='relative'>
-        <label className='input-label'>Message</label>
-        <textarea
-          rows={10}
-          className='contact-input py-3'
-          placeholder='Your message here!!!'
-          name='message'
-          onChange={onInputChange}
-        ></textarea>
-        <span className='absolute right-1 -top-5 text-sm text-red-600 font-semibold'>
-          {formErrors.message}
-        </span>
-      </div>
+      {(props) => (
+        <form
+          className='flex flex-col gap-7 justify-around md:col-span-2 h-full'
+          onSubmit={props.handleSubmit}
+        >
+          <div className='relative'>
+            <label className='input-label'>Name</label>
+            <Field
+              name='name'
+              type='text'
+              placeholder='Your name'
+              value={props.values.name}
+              className='contact-input'
+              onChange={props.handleChange}
+            />
+            <span className='absolute right-1 -top-5 text-sm text-red-600 font-semibold'></span>
+          </div>
+          <div className='relative'>
+            <label className='input-label'>Email</label>
+            <Field
+              name='email'
+              type='email'
+              placeholder='email@example.com'
+              value={props.values.email}
+              className='contact-input'
+              onChange={props.handleChange}
+            />
+            <span className='absolute right-1 -top-5 text-sm text-red-600 font-semibold'></span>
+          </div>
+          <div className='relative'>
+            <label className='input-label'>Message</label>
+            <Field
+              as='textarea'
+              rows={10}
+              name='message'
+              placeholder='Your message here!!!'
+              value={props.values.message}
+              className='contact-input py-3'
+              onChange={props.handleChange}
+            />
 
-      <button
-        className='
-          bg-[#4c837a] 
-          hover:bg-[#24403b]
-          rounded-xl
-          h-11
-          text-white
-          font-bold
-          text-center
-          w-full
-        '
-        type='submit'
-      >
-        Send Message
-      </button>
-    </form>
+            <span className='absolute right-1 -top-5 text-sm text-red-600 font-semibold'></span>
+          </div>
+
+          <button
+            className='
+              bg-[#4c837a] 
+              hover:bg-[#24403b]
+              rounded-xl
+              h-11
+              text-white
+              font-bold
+              text-center
+              w-full
+            '
+            type='submit'
+          >
+            Send Message
+          </button>
+        </form>
+      )}
+    </Formik>
   );
 };
 
