35352cb7b1825cc13b6d49eb934f44cf6d95fa66
LugpDev2022 <lugpdev2022@gmail.com>
2023-06-13 13:54:44 -0600

    Fix visual footer bug

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,8 +4,9 @@ import Project from '@/components/Project';
 import projectsArray from '@/projectsArray';
 
 const page = () => (
-  <div
-    className={`
+  <div className='min-h-[calc(100vh-112px)] flex items-center justify-center'>
+    <div
+      className={`
         animate__animated
         animate__fadeIn
         flex
@@ -17,11 +18,12 @@ const page = () => (
         lg:p-10
         lg:gap-10
       `}
-  >
-    <h3 className='subtitle sm:hidden w-full text-center'>Portfolio</h3>
-    {projectsArray.map((project, i) => (
-      <Project key={project.title} {...project} />
-    ))}
+    >
+      <h3 className='subtitle sm:hidden w-full text-center'>Portfolio</h3>
+      {projectsArray.map((project, i) => (
+        <Project key={project.title} {...project} />
+      ))}
+    </div>
   </div>
 );
 
