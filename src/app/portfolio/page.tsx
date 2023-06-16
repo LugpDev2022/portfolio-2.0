3ce76136acc942e2b0b117711676cb0738446ec3
LugpDev2022 <lugpdev2022@gmail.com>
2023-06-15 18:26:16 -0600

    Improve portfolio layout

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -4,25 +4,23 @@ import Project from '@/components/Project';
 import projectsArray from '@/projectsArray';
 
 const Portfolio = () => (
-  <main className='min-h-[calc(100vh-112px)] flex items-center justify-center'>
-    <div
-      className={`
-        animate__animated
-        animate__fadeIn
-        flex
-        flex-row
-        flex-wrap
-        justify-center
-        p-7
-        gap-7
-        lg:p-10
-        lg:gap-10
-      `}
-    >
+  <main className='min-h-[calc(100vh-112px)] flex items-center justify-center animate__animated animate__fadeIn'>
+    <div className='p-7 lg:p-10 basis-full xl:basis-4/5'>
       <h3 className='subtitle sm:hidden w-full text-center'>Portfolio</h3>
-      {projectsArray.map((project, i) => (
-        <Project key={project.title} {...project} />
-      ))}
+      <div
+        className={`
+        grid
+        grid-cols-1
+        justify-items-center
+        gap-7
+        sm:grid-cols-2
+        lg:grid-cols-3
+      `}
+      >
+        {projectsArray.map((project, i) => (
+          <Project key={project.title} {...project} />
+        ))}
+      </div>
     </div>
   </main>
 );
